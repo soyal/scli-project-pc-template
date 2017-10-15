c90f484b3cb0ebbe099f5b31eeca78e2348491c6
Soyal <345723940@qq.com>
2017-10-15 09:24:59 +0800

    project init

---
+++ src/pages/demo/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Demo extends Component {
+  render() {
+    <div>
+      this is demo
+    </div>
+  }
+}
+
+export default Demo
---
+++ src/routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+const Routes = () => {
+  return (
+    <Router>
+      
+    </Router>
+  )
+}
+
+export default Routes
